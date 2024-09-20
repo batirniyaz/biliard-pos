b1cd4cab809f9bf49c3d2f4fdd7ae64144f2a5ce
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-20 09:44:25 +0500

    include product and option routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 from app.table.api import router as table_router
+from app.menu.product.api import router as product_router
+from app.menu.option.api import router as option_router
 
 router = APIRouter()
 
 router.include_router(table_router, prefix="/table", tags=["Table"])
+router.include_router(product_router, prefix="/product", tags=["Product"])
+router.include_router(option_router, prefix="/option", tags=["Option"])
 
