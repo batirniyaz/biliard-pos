8945c9451a4f4a6ad47e72887915f81a08546581
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-21 12:37:26 +0500

    add extra fields for table info in order

--- app/order/model.py
+++ app/order/model.py
@@ -1,8 +1,8 @@
 import datetime
 
-from typing import List, Dict
+from typing import List
 
-from sqlalchemy import Integer, String, JSON, Float, TIMESTAMP, Boolean
+from sqlalchemy import Integer, String, JSON, Float, TIMESTAMP, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.auth.database import Base
@@ -12,7 +12,9 @@ class Order(Base):
     __tablename__ = "order"
 
     id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
-    table_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    table_id: Mapped[int] = mapped_column(Integer, ForeignKey("table.id"), nullable=False)
+    table_name: Mapped[str] = mapped_column(String(length=255), nullable=True)
+    table_status: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
     status: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
     start_time: Mapped[str] = mapped_column(String(length=255), nullable=True)
     products: Mapped[List] = mapped_column(JSON, default=[])
