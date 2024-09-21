31e481602e4382ac1974920a3f62b5af821b7718
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-21 15:58:05 +0500

    add delete endpoint to order

--- app/order/api.py
+++ app/order/api.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.auth.database import get_async_session
 
 from app.order.schema import OrderCreate, OrderResponse, OrderUpdate
-from app.order.crud import create_order, update_order, get_order, get_orders
+from app.order.crud import create_order, update_order, get_order, get_orders, delete_order
 
 router = APIRouter()
 
@@ -40,3 +40,11 @@ async def update_order_endpoint(
         db: AsyncSession = Depends(get_async_session)
 ):
     return await update_order(db, order_id, order)
+
+
+@router.delete("/{order_id}")
+async def delete_order_endpoint(
+        order_id: int,
+        db: AsyncSession = Depends(get_async_session)
+):
+    return await delete_order(db, order_id)
