125efde83f76c633d6c2bd881b3a6a5a44986f5a
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-23 09:53:38 +0500

    load postgresql configs

--- app/config.py
+++ app/config.py
@@ -5,3 +5,14 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET")
 TG_TOKEN = os.getenv("TG_TOKEN")
+TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASS = os.getenv("POSTGRES_PASS")
