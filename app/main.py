c07b787b3868b01f0805a6a582b55a490999c3cc
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-23 14:49:48 +0500

    import host and port of redis and use

--- app/main.py
+++ app/main.py
@@ -13,12 +13,14 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
+from app.config import REDIS_HOST, REDIS_PORT
+
 
 @asynccontextmanager
 async def lifespan(main_app: FastAPI):
     await create_db_and_tables()
 
-    redis = aioredis.from_url("redis://localhost")
+    redis = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}", encoding="utf-8", decode_responses=True)
     FastAPICache.init(RedisBackend(redis), prefix="biliard-cache")
 
     yield
