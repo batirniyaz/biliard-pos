aaa0f3f3668cd8b512a20efd1df5c8641fc52ab8
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-23 14:50:01 +0500

    add host and port of redis to config.py

--- app/config.py
+++ app/config.py
@@ -16,3 +16,6 @@ DB_NAME = os.getenv("DB_NAME")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASS = os.getenv("POSTGRES_PASS")
+
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
