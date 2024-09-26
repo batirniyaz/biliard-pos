949e37f1010f23b9b138168db342bbe84b2b3c1d
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-26 15:46:49 +0500

    add report endpoint to router

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from app.table.api import router as table_router
 from app.menu.product.api import router as product_router
 from app.menu.option.api import router as option_router
 from app.order.api import router as order_router
+from app.daily_report.api import router as report_router
 
 router = APIRouter()
 
@@ -10,4 +11,6 @@ router.include_router(table_router, prefix="/table", tags=["Table"])
 router.include_router(product_router, prefix="/product", tags=["Product"])
 router.include_router(option_router, prefix="/option", tags=["Option"])
 router.include_router(order_router, prefix="/order", tags=["Order"])
+
+router.include_router(report_router, prefix="/report", tags=["Report"])
 
