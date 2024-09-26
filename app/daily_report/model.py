5037bd3180c7225b84a4c33301d854154537c513
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-26 15:47:06 +0500

    reports models: table and day

---
+++ app/daily_report/model.py
@@ -0,0 +1,40 @@
+import datetime
+from typing import List
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, TIMESTAMP, JSON
+from app.auth.database import Base
+
+
+class DailyReport(Base):
+    __tablename__ = "daily_report"
+
+    id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
+    date: Mapped[str] = mapped_column(String(length=10), nullable=False)
+    total_income: Mapped[int] = mapped_column(Integer, nullable=False)
+    table_income: Mapped[int] = mapped_column(Integer, nullable=False)
+    product_income: Mapped[int] = mapped_column(Integer, nullable=False)
+    products: Mapped[List] = mapped_column(JSON, default=[])
+    total_play_time: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
+
+
+class TableReport(Base):
+    __tablename__ = "table_report"
+
+    id: Mapped[int] = mapped_column(Integer, unique=True, index=True, nullable=False, primary_key=True)
+    table_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    products: Mapped[List] = mapped_column(JSON, default=[])
+    total_income: Mapped[int] = mapped_column(Integer, nullable=False)
+    total_play_time: Mapped[int] = mapped_column(Integer, nullable=False)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
