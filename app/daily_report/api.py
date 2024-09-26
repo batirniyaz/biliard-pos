ca9e64291be18c44c93ac237df16c51d1a5ab189
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-09-26 15:47:47 +0500

    get apis of table and day reports

---
+++ app/daily_report/api.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import List
+
+from fastapi import APIRouter, Depends, Query
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.database import get_async_session
+from app.daily_report.schema import DailyReportResponse
+from app.daily_report.crud import get_daily_report, calculate_table_report
+from app.utils.time_utils import get_time
+
+router = APIRouter()
+
+
+@router.get("/daily")
+async def get_daily_report_endpoint(
+        date: str = Query(
+            None,
+            description="The date of the daily report",
+            alias="daily_report_date"),
+        db: AsyncSession = Depends(get_async_session)
+):
+    if date is None:
+        _, date = await get_time()
+    return await get_daily_report(db, date)
+
+
+@router.get("/table")
+async def get_table_report_endpoint(
+        date: str = Query(
+            None,
+            description="The date of the daily report",
+            alias="daily_report_date"),
+        table_id: int = Query(..., description="The id of the table", alias="table_id"),
+        db: AsyncSession = Depends(get_async_session)
+
+):
+    if date is None:
+        _, date = await get_time()
+    return await calculate_table_report(db, date, table_id)
