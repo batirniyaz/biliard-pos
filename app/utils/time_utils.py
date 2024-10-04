748aeee965563a239cb98229339478220b720068
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-04 16:44:38 +0500

    add one more func: get_time_sync

--- app/utils/time_utils.py
+++ app/utils/time_utils.py
@@ -12,7 +12,18 @@ async def get_time():
         uzb_time = time.localtime(response.tx_time)
         time_string = time.strftime("%H:%M:%S", uzb_time)
         date = time.strftime("%Y-%m-%d", uzb_time)
-        return time_string, date
+        return time_string, date, uzb_time
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="Failed to get time from NTP server")
+
+
+def get_time_sync():
+    try:
+        client = ntplib.NTPClient()
+        response = client.request('uz.pool.ntp.org', version=3)
+        uzb_time = time.localtime(response.tx_time)
+        return uzb_time
     except Exception:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail="Failed to get time from NTP server")
