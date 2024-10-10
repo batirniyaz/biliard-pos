5d5b86b88dd1606836f48d60b1b26a8564a148ad
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-10-10 11:29:22 +0500

    add two endpoints: session_history and print_check

--- app/daily_report/api.py
+++ app/daily_report/api.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.auth.database import get_async_session
 from app.daily_report.crud import get_daily_report, get_table_report, close_session
+from app.utils.check_util import history_check, check_helper
 
 router = APIRouter()
 
@@ -38,3 +39,18 @@ async def close_session_endpoint(
         db: AsyncSession = Depends(get_async_session)
 ):
     return await close_session(db)
+
+
+@router.get("/session_history")
+async def session_history_endpoint(
+        db: AsyncSession = Depends(get_async_session)
+):
+    return await history_check(db)
+
+
+@router.get("/print_check")
+async def print_check_endpoint(
+        order_id: int = Query(..., description="The id of the order", alias="order_id"),
+        db: AsyncSession = Depends(get_async_session)
+):
+    return await check_helper(db, order_id)
